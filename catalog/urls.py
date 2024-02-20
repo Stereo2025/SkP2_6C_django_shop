6e6bf725c8aa895a6e4814e8e974cf7b7273094d
Stereo2025 <teneloks@gmail.com>
2024-02-20 19:00:01 +0300

    20.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,13 @@
-from catalog.views import home_page, contact
+from catalog.apps import CatalogConfig
+from catalog.views import home_page, contacts, products, user_product, view_product
 from django.urls import path
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', home_page),
-    path('contacts/', contact)
+    path('', home_page, name='home_page'),
+    path('contacts/', contacts, name='contacts'),
+    path('products/', products, name='products'),
+    path('add/', user_product, name='user_product'),
+    path('products/<int:pk>/', view_product, name='view_product')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,67 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from catalog.models import Product, Contact, Category
 
 
-# Create your views here.
 def home_page(request):
-    return render(request, 'catalog/home_page.html')
+    """Контроллер домашней страницы"""
+    products = Product.objects.all()
+    last_products = []
+    for item in reversed(products):
+        last_products.append(item)
+    context = {
+        'product_list': last_products[:5],
+    }
+
+    return render(request, 'catalog/home_page.html', context=context)
 
 
-def contact(request):
+def contacts(request):
+    """Контроллер страницы контактов"""
+    contacts = Contact.objects.all()
+    data = {
+        'contact_list': contacts,
+    }
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have a message from {name}({phone}): {message}')
-    return render(request, 'catalog/contact.html')
+    return render(request, 'catalog/contact.html', context=data)
+
+
+def products(request):
+    """Контроллер страницы товаров"""
+    products_list = Product.objects.all()
+    paginator = Paginator(products_list, 6)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
+    context = {
+        'product_list': page_obj,
+    }
+    return render(request, 'catalog/products.html', context=context)
+
+
+def user_product(request):
+    """Контроллер страницы добавления товара от пользователя"""
+    category_list = Category.objects.all()
+    context = {
+        'category_list': category_list,
+    }
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        image = request.FILES.get('image')
+        price = request.POST.get('price')
+        category = Category.objects.get(name=request.POST.get('category'))
+        print(f'1 - {name}, 2 - {description}, 3 - {image}, 4 - {price}, 5 - {category}')
+        Product.objects.create(name=name, description=description, image=image, price=price, category=category)
+    return render(request, 'catalog/user_product.html', context=context)
+
+
+def view_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {
+        'product': product
+    }
+    return render(request, 'catalog/view_product.html', context=context)
